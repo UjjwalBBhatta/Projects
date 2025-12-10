e2efbe7208186fda0ed3272df90b80b1e262dcf8
UjjwalBBhatta <ujjwal.117320@mitnepal.edu.np>
2025-12-10 12:48:53 +0545

    Created a module to get the html from the user given URL

---
+++ Web Scraper - Project 5/src/get_html.py
@@ -0,0 +1,19 @@
+import requests
+def get_html(url):
+    """This function returns the HTML content of the given URL.
+    Args:
+        url (str): The URL of the webpage to fetch.
+        Returns:
+        str: The HTML content of the webpage, or None if an error occurs."""
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an error for bad responses
+        return response.text
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        return None
+   
+    
+    
+    
+
