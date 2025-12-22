ec5a35c213134d64f8877b0e1152986dbdb9d50f
UjjwalBBhatta <ujjwal.117320@mitnepal.edu.np>
2025-12-22 19:45:55 +0545

    Created a module to delete the receivers in the json file

---
+++ Email Automation - Project 6/src/del_receiver.py
@@ -0,0 +1,26 @@
+import json
+
+def del_receiver(email):
+    """This function deletes a receiver from the receivers.json file.
+    It first checks if the receiver exists, and
+    then removes this information from the JSON file.
+    """
+    # Load existing receivers
+    try:
+        with open('receivers.json', 'r') as file:
+            receivers = json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        print("No receivers found.")
+        return
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return
+    # Check if receiver exists and remove
+    for receiver in receivers['receivers']:
+        if receiver['email'] == email:
+            receivers['receivers'].remove(receiver)
+            with open('receivers.json', 'w') as file:
+                json.dump(receivers, file, indent=4)
+            print(f"Receiver with email {email} deleted successfully.")
+            return
+    print(f"No receiver found with email {email}.")
