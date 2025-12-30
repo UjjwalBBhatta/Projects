4b9d6d802a35e492efc4084b4e90aa8a3d8cb88d
UjjwalBBhatta <ujjwal.117320@mitnepal.edu.np>
2025-12-30 13:00:16 +0545

    Integrated tracking into the instance functions

--- Bank Account Simulator - Project 6/account.py
+++ Bank Account Simulator - Project 6/account.py
@@ -1,17 +1,22 @@
+from src.transaction_history import add_transaction
 
 class Account():
     """Creating the main parent class Account"""
     def __init__(self, account_id, balance=0):
         self.account_id = account_id
         self.balance = balance
-    def deposit(self, amount):
+    # Now we require a category when depositing
+    def deposit(self, amount, category="General"):
         if amount > 0:
             self.balance += amount
+            add_transaction(amount, category, f"Deposit to {self.account_id}")
             return True
         return False
     def withdraw(self, amount):
         if 0 < amount <= self.balance:
             self.balance -= amount
+            # Pass the Category and Description!
+            add_transaction(amount, "Withdrawal", f"Withdrew from {self.account_id}")
             return True
         return False
     def get_balance(self):
@@ -21,8 +26,8 @@ class Account():
     def __str__(self):
         return f"Account ID: {self.account_id}, Balance: {self.balance}"
     def transfer(self, target_account, amount):
-        if self.withdraw(amount):
-            target_account.deposit(amount)
+        if self.withdraw(amount): 
+            target_account.deposit(amount, "Transfer") 
             return True
         return False
     def card_linked(self, card_number):
@@ -50,6 +55,7 @@ class CheckingAccount(Account):
     def withdraw(self, amount):
         if 0 < amount <= self.balance + self.overdraft_limit:
             self.balance -= amount
+            add_transaction(amount, "Withdrawal", f"Overdraft Withdrawal {self.account_id}")
             return True
         return False
     def __str__(self):
