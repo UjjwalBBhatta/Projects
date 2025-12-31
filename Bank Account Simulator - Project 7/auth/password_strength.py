e6c29745b14cde480b9403af783dccbd6f698cfe
UjjwalBBhatta <ujjwal.117320@mitnepal.edu.np>
2025-12-31 14:12:05 +0545

    Created a basic password strength c heck

---
+++ Bank Account Simulator - Project 7/auth/password_strength.py
@@ -0,0 +1,23 @@
+def check_strength(password):
+    """Check password strength (simple version)"""
+    score = 0
+    
+    if len(password) >= 8:
+        score += 1
+    if len(password) >= 12:
+        score += 1
+    if any(c.isupper() for c in password):
+        score += 1
+    if any(c.islower() for c in password):
+        score += 1
+    if any(c.isdigit() for c in password):
+        score += 1
+    if any(c in '!@#$%^&*' for c in password):
+        score += 1
+    
+    if score <= 2:
+        return "Weak"
+    elif score <= 4:
+        return "Medium"
+    else:
+        return "Strong"
